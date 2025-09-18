a70ac6499948f19da39a1f289793a1fe58ec06d4
Will D <willdenford.dev@gmail.com>
2025-09-18 21:14:56 +1000

    start of next app

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,13 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
@@ -19,7 +19,23 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+       // Syntax consistency rules
+      "semi": ["error", "never"],
+      "quotes": ["error", "double", { "avoidEscape": true }],
+      "jsx-quotes": ["error", "prefer-double"],
+      "comma-dangle": ["error", "always-multiline"],
+      "object-curly-spacing": ["error", "always"],
+      "array-bracket-spacing": ["error", "never"],
+      "computed-property-spacing": ["error", "never"],
+      "space-before-blocks": ["error", "always"],
+      "keyword-spacing": ["error", { "before": true, "after": true }],
+      "space-infix-ops": "error",
+      "eol-last": ["error", "always"],
+      "no-trailing-spaces": "error",
+      "linebreak-style": ["error", "unix"],
+    },
   },
-];
+]
 
-export default eslintConfig;
+export default eslintConfig
